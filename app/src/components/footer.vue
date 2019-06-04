b44e49b357470e484935b4cdaa13c0606b589043
3166118 <xye@ocadu.ca>
2019-06-04 16:05:47 -0400

    start

---
+++ app/src/components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>footer</p>
+    <button type="button" name="button">make an appointment</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Footer",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ app/src/components/nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About Dr. Ye</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Nav",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
